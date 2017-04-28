3595686148cbe35cf6210cf200071092f6332482
Klaus Förster <openwebcc@github.com>
2017-04-28 19:11:53 +0200

    add script source

---
+++ src/GeoJSONFromURL.js
@@ -0,0 +1,60 @@
+/*
+ * 
+ * load GeoJSON data directly from a url
+ * 
+ */
+
+L.GeoJSONFromURL = L.GeoJSON.extend({
+    // set defaults for plugin options
+    myOpts: {
+        url : null,
+        async : true,
+        callback : function(map) {
+            return undefined;
+        },
+    },
+
+    callback: function(geojsondata) {
+        // add GeoJSON data
+        this.addData(geojsondata);
+
+        // run optional callback
+        this.myOpts.callback(this);
+    },
+
+    initialize: function(opts, options) {
+        // assign plugin options
+        for (var o in this.myOpts) {
+            if (this.myOpts.hasOwnProperty(o) && opts.hasOwnProperty(o)) {
+                this.myOpts[o] = opts[o];
+            }
+        }
+
+        // call parents constructor
+        L.GeoJSON.prototype.initialize.call(this, undefined, options);
+    },
+
+    loadURL: function() {
+        // get GeoJSON data from given url with XMLHttpRequest
+        var self = this;
+        var request = new XMLHttpRequest();
+        request.open("GET", this.myOpts.url, this.myOpts.async);
+        request.onload = function() {
+            if (this.status >= 200 && this.status < 400) {
+                self.callback(JSON.parse(this.responseText));
+            }
+        };
+        request.send();
+    },
+
+    onAdd: function(map) {
+        if (this.myOpts.url) {
+            this.loadURL();
+        }
+    },
+});
+
+// add factory function
+L.geoJSONFromURL = function(opts, options) {
+    return new L.GeoJSONFromURL(opts, options);
+};
